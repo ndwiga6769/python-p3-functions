69d0f49a9f3ed4a1bf5afe659e8b2f8e76d7e43f
danwycliff <danwycliff6769@gmail.com>
2023-09-11 21:57:06 +0300

    codes

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,20 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
-def greet(name):
+def greet(name = "Guido"):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
-
+    print(f"Hello, {name}!")
 def add(num1, num2):
     pass
-
+    return num1 + num2
+add(45,55)
 def halve(number):
     pass
+    return number/2
+halve(20)
